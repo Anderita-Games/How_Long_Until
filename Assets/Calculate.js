8786a83606cecabe495ea1dd57cad7e7d64f9967
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-16 19:47:26 -0500

    Added App
    This is the first version of How Long Until that GitHub has ever seen!

---
+++ Assets/Calculate.js
@@ -0,0 +1,53 @@
+﻿#pragma strict
+@Header ("All the Text")
+var Year : UnityEngine.UI.Text;
+var Month : UnityEngine.UI.Text;
+var Day : UnityEngine.UI.Text;
+var Hours : UnityEngine.UI.Text;
+var Minutes : UnityEngine.UI.Text;
+var Seconds : UnityEngine.UI.Text;
+var Title : UnityEngine.UI.Text;
+
+@Space (10)
+
+@Header ("Current Time")
+var Year2 : int;
+var Month2 : int;
+var Day2 : int;
+var Hours2 : int;
+var Minutes2 : int;
+var Seconds2 : int;
+
+function Start () {
+	Title.text = "Time until " + PlayerPrefs.GetString("EventName") + ":";
+}
+
+function Update () {
+	Debug.Log(System.DateTime.UtcNow.ToString("ss"));
+	Year2 = parseFloat(System.DateTime.UtcNow.ToString("yyyy"));
+	Month2 = parseFloat(System.DateTime.UtcNow.ToString("MM"));
+	Day2 = parseFloat(System.DateTime.UtcNow.ToString("dd"));
+	Hours2 = parseFloat(System.DateTime.UtcNow.ToString("HH"));
+	Minutes2 = parseFloat(System.DateTime.UtcNow.ToString("mm"));
+	Seconds2 = parseFloat(System.DateTime.UtcNow.ToString("ss"));
+	
+	var Seconds3 = Mathf.Abs((((PlayerPrefs.GetFloat("Year") - Year2) * 365) + ((PlayerPrefs.GetFloat("Month") - Month2) * 30) + (PlayerPrefs.GetFloat("Day") - Day2)) * 86400 + (Hours2 * 3600) + (Minutes2 * 60) + (60 - Seconds2));
+	
+	Year.text = Mathf.Abs((PlayerPrefs.GetFloat("Year") - Year2) + ((PlayerPrefs.GetFloat("Month") - Month2) / 12) + ((PlayerPrefs.GetFloat("Day") - Day2) / 365)).ToString("n0") + " Years";
+	
+	Month.text = Mathf.Abs(((PlayerPrefs.GetFloat("Year") - Year2) * 12) + (PlayerPrefs.GetFloat("Month") - Month2) + ((PlayerPrefs.GetFloat("Day") - Day2) / 30)).ToString("n0") + " Months";
+	
+	Day.text = Mathf.Abs(((PlayerPrefs.GetFloat("Year") - Year2) * 365) + ((PlayerPrefs.GetFloat("Month") - Month2) * 30) + (PlayerPrefs.GetFloat("Day") - Day2)).ToString("n0") + " Days";
+	
+	Hours.text = Mathf.Abs((((PlayerPrefs.GetFloat("Year") - Year2) * 365) + ((PlayerPrefs.GetFloat("Month") - Month2) * 30) + (PlayerPrefs.GetFloat("Day") - Day2)) * 24 + Hours2).ToString("n0") + " Hours";
+	
+	Minutes.text = Mathf.Abs((((PlayerPrefs.GetFloat("Year") - Year2) * 365) + ((PlayerPrefs.GetFloat("Month") - Month2) * 30) + (PlayerPrefs.GetFloat("Day") - Day2)) * 1440 + (Hours2 * 60) + Minutes2).ToString("n0") + " Minutes";
+	
+	Seconds.text = Seconds3.ToString("n0") + " Seconds";
+}
+
+function Reset () {
+	PlayerPrefs.DeleteAll();
+	Application.LoadLevel ("MainMenu");
+}
+
---
+++ Assets/Number.js
@@ -0,0 +1,61 @@
+﻿#pragma strict
+@Header ("Title")
+var Title : UnityEngine.UI.Text;
+
+@Space (10)
+
+@Header ("InputFields")
+var EventName : UnityEngine.UI.InputField;
+var Month : UnityEngine.UI.InputField;
+var Day : UnityEngine.UI.InputField;
+var Year : UnityEngine.UI.InputField;
+
+function Start () {
+	yield WaitForSeconds (.1);
+	Submit();
+	Title.color = new Color(1.0f, 1.0f, 1.0f);
+}
+
+function Submit () {
+	if (PlayerPrefs.GetFloat("Month") != null && PlayerPrefs.GetFloat("Day") != null && PlayerPrefs.GetFloat("Year") != null) {
+		if (PlayerPrefs.GetFloat("Month") > 12 || PlayerPrefs.GetFloat("Month") < 1) {
+			Title.color = new Color(1.0f, 0.0f, 0.0f);
+			Title.text == "INVALID MONTH";
+		}else if (PlayerPrefs.GetFloat("Day") > 31 || PlayerPrefs.GetFloat("Day") < 1) {
+			Title.color = new Color(1.0f, 0.0f, 0.0f);
+			Title.text == "INVALID DAY";
+		}else if (PlayerPrefs.GetFloat("Year") < parseFloat(System.DateTime.UtcNow.ToString("yyyy"))) {
+			Title.color = new Color(1.0f, 0.0f, 0.0f);
+			Title.text == "INVALID YEAR";
+		}else {
+			Application.LoadLevel ("Stats");
+		}
+	}else if (PlayerPrefs.GetFloat("Month") == null) {
+		Title.color = new Color(1.0f, 0.0f, 0.0f);
+		Title.text == "INSERT MONTH";
+	}else if (PlayerPrefs.GetFloat("Day") == null) {
+		Title.color = new Color(1.0f, 0.0f, 0.0f);
+		Title.text == "INSERT DAY";
+	}else if (PlayerPrefs.GetFloat("Year") == null) {
+		Title.color = new Color(1.0f, 0.0f, 0.0f);
+		Title.text == "INSERT YEAR";
+	}else {
+		Debug.Log("ERROR WITH SUBMITING");
+	}
+}
+
+function EventChanger () {
+	PlayerPrefs.SetString("EventName", EventName.text);
+}
+
+function Months () {
+	PlayerPrefs.SetFloat("Month", int.Parse(Month.text));
+}
+
+function Days () {
+	PlayerPrefs.SetFloat("Day", int.Parse(Day.text));
+}
+
+function Years () {
+	PlayerPrefs.SetFloat("Year", int.Parse(Year.text));
+}
